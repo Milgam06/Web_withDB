1f12c906c314c3a294adf232085af03933c3b706
Milgam06 <milgamfruit@gmail.com>
2022-10-30 23:18:46 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   .DS_Store
    	new file:   code/__pycache__/db.cpython-310.pyc
    	new file:   code/__pycache__/main.cpython-310.pyc
    	renamed:    db.py -> code/db.py
    	renamed:    main.py -> code/main.py
    	renamed:    db01.spf -> database/db01.spf

--- code/db.py
+++ code/db.py
@@ -1,22 +1,33 @@
 import pymysql
-
+import pandas
+import json
+import jwt
 
 # milgamDB connect
-milgam_db = pymysql.connect(
+milgamDB = pymysql.connect(
     user = "root",
     password = "12345678",
     host = "127.0.0.1",
-    db = "milgamDB",
+    db = "milgam_db",
     charset = "utf8mb4"
 )
 
 
 # make cursor what selecting SQL objects
-cursor = milgam_db.cursor()
+cursor = milgamDB.cursor()
 
 
 # data lookup & start running SQL
 sql = "SELECT * FROM mandarinDB;"
 cursor.execute(sql)
 result = cursor.fetchall()
-print(*result)
+
+
+# # transform to dataframe
+result = pandas.DataFrame(result)
+print(result)
+
+
+
+sql = '''INSERT INTO mandarinDB (id, name, email)
+    Value ('3', ');'''
--- code/main.py
+++ code/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from typing import Union
+import db
+
 
 app = FastAPI()
 
-@app.post('/')
+@app.get('/')
 def read_root():
     return {"Hello: world!"}
 
-@app.post("/items/{items_id}")
+@app.get("/items/{items_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/names")
+def read_names():
+    return db.read_name()
